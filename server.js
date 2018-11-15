f03b3c6d7db69ce1527a0205e813a79986079cc8
CB_ATL_118_Noah_Goodman <cb_atl_118_noah_goodman@MacBook-Air-2.local>
2018-11-15 13:54:00 -0500

    I added more things to the server.js that made the connedctions to mongoose

--- server.js
+++ server.js
@@ -4,3 +4,16 @@ const mongoose = require('mongoose')
 const app = express()
 mongoose.connect(process.env.MONGODB_URI)
 
+const connection = mongoose.connection
+connection.on('connected', () => {
+    console.log('mongoose is connected')
+})
+
+connection.on('error', (err) => {
+    console.log('Mongoose default connection error: ' + err);
+  }) 
+
+  const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Magic happening on port " + PORT);
+})
