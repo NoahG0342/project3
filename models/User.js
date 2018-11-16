79cca429bcf0c8069c3ff207e07f664026c41f87
CB_ATL_118_Noah_Goodman <cb_atl_118_noah_goodman@MacBook-Air-2.local>
2018-11-16 12:38:35 -0500

    finished my model for User

--- models/User.js
+++ models/User.js
@@ -1,1 +1,15 @@
-const mongoose = require()
+const mongoose = require('../db/connections')
+const Schema = mongoose.Schema
+
+const User = new Schema ({
+    username: String,
+    password: String,
+    age: Number,
+    lp: Number,
+    decks: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Deck'
+    }]
+})
+
+module.exports = mongoose.model('User', User)
