d143839457dbb64b43651429cabf1209f136d0e2
CB_ATL_118_Noah_Goodman <cb_atl_118_noah_goodman@MacBook-Air-2.local>
2018-11-16 13:43:38 -0500

    did the first 3 seeds for the monster cards

--- db/seeds.js
+++ db/seeds.js
@@ -2,3 +2,27 @@ const User = require('../models/User')
 const Deck = require('../models/Deck')
 const Cards = require('../models/Cards')
 
+const blue = new Cards({
+    name: 'Blue-Eyes White Dragon',
+    type: 'monster',
+    ATK: 3000,
+    DEF: 2500,
+    discription: 'This legendary dragon is a powerful engin of destruction'
+})
+
+const dark = new Cards({
+    name: 'Dark Magician',
+    type: 'monster',
+    ATK: 2500,
+    DEF: 2100,
+    discription: 'The ultimate wizard in terms of ATK and Def'
+})
+
+const red = new Cards({
+    name: 'Red-Eyes Black Dragon',
+    type: 'monster',
+    ATK: 2400,
+    DEF: 2000,
+    discription: 'a strong drangon with a deadly attack'
+})
+
